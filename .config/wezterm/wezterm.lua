4fd8fbc23b622715b71605b71067888512507260
toma09to <contact@mail.toma09to.com>
2024-11-25 19:03:36 +0900

    Disable fullscreen in Windows

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,13 +1,19 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+local is_windows = function()
+  return wezterm.target_triple:find('windows') ~= nil
+end
+
 config.window_background_opacity = 0.80
 
-local mux = wezterm.mux
-wezterm.on("gui-startup", function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():toggle_fullscreen()
-end)
+if not is_windows then
+  local mux = wezterm.mux
+  wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():toggle_fullscreen()
+  end)
+end
 
 config.font = wezterm.font_with_fallback {
   'MesloLGS NF',
@@ -18,7 +24,10 @@ config.color_scheme = 'iceberg-dark'
 -- config.window_background_image_hsb = {
   -- brightness = 0.05,
 -- }
-config.window_decorations = "RESIZE"
+
+if not is_windows then
+  config.window_decorations = 'RESIZE'
+end
 config.disable_default_mouse_bindings = true
 
 return config
