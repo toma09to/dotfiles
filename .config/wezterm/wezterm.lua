0d1bdd647aea7b038884e6da1496f7b8b0c39d66
toma09to <toma09to@mail.toma09to.com>
2024-11-30 21:04:30 +0900

    Edit wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,15 +4,24 @@ local config = wezterm.config_builder()
 local is_windows = function()
   return wezterm.target_triple:find('windows') ~= nil
 end
+local is_mac = function()
+  return wezterm.target_triple:find('apple') ~= nil
+end
 
 config.window_background_opacity = 0.80
 
-if not is_windows then
+-- Fullscreen in MacBook
+if is_mac then
   local mux = wezterm.mux
   wezterm.on('gui-startup', function(cmd)
     local tab, pane, window = mux.spawn_window(cmd or {})
     window:gui_window():toggle_fullscreen()
   end)
+end
+
+-- Change the default domain into WSL in Windows
+if is_windows then
+  config.defualt_domain = 'WSL:Ubuntu-24.04'
 end
 
 config.font = wezterm.font_with_fallback {
