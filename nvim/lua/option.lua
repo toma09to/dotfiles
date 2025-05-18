60930d1bdf2c16d8ebc890cd60b47c41249f9dc0
toma09to <toma09to@mail.toma09to.com>
2025-05-19 08:53:41 +0900

    Initialize Neovim config

---
+++ nvim/lua/option.lua
@@ -0,0 +1,20 @@
+local options = {
+    -- encoding
+    encoding = 'utf-8',
+    fileencoding = 'utf-8',
+    -- line number
+    number = true,
+    relativenumber = true,
+    -- disabling mouse
+    mouse = '',
+    -- indent
+    expandtab = true,
+    tabstop = 4,
+    shiftwidth = 4,
+    smartindent = true,
+    smarttab = true,
+}
+
+for k,v in pairs(options) do
+    vim.opt[k] = v
+end
